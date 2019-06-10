becbcaf49461bf4e510b7c0118a4b2ec5c58f6b2
Scc <sccong@outlook.com>
2019-06-10 16:30:03 +0800

    refactor profile to hooks

--- src/renderer/container/profile/index.tsx
+++ src/renderer/container/profile/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 import { remote } from 'electron';
 import { IState } from '@/renderer/context';
@@ -6,11 +6,38 @@ import consumer from '@/renderer/context/consumer';
 
 interface Props extends RouteComponentProps, IState { }
 
-@(consumer as any)
-@(withRouter as any)
-export default class Profile extends React.Component<Props> {
-  private chooseWorkPlace = () => {
-    const { projectUrls, setProjectUrls } = this.props;
+// @(consumer as any)
+// @(withRouter as any)
+// export default class Profile extends React.Component<Props> {
+//   private chooseWorkPlace = () => {
+//     const { projectUrls, setProjectUrls } = this.props;
+//     const { dialog } = remote;
+//     dialog.showOpenDialog({
+//       properties: [
+//         'openDirectory',
+//       ],
+//     },
+//       (filePaths) => {
+//         setProjectUrls(`${projectUrls},${filePaths}`);
+//       });
+//   }
+
+//   public render() {
+//     const { history } = this.props;
+
+//     return (
+//       <div>
+//         <button onClick={() => { history.goBack(); }}>返回</button>
+//         <button onClick={() => this.chooseWorkPlace()}>添加项目</button>
+//       </div>
+
+//     );
+//   }
+// }
+
+
+const Profile2: React.FC<Props> = ({ projectUrls, setProjectUrls, history }) => {
+  const chooseWorkPlace = useCallback(() => {
     const { dialog } = remote;
     dialog.showOpenDialog({
       properties: [
@@ -20,17 +47,14 @@ export default class Profile extends React.Component<Props> {
       (filePaths) => {
         setProjectUrls(`${projectUrls},${filePaths}`);
       });
-  }
+  }, []);
 
-  public render() {
-    const { history } = this.props;
+  return (
+    <div>
+      <button onClick={() => { history.goBack(); }}>返回</button>
+      <button onClick={() => chooseWorkPlace()}>添加项目</button>
+    </div>
+  );
+};
 
-    return (
-      <div>
-        <button onClick={() => { history.goBack(); }}>返回</button>
-        <button onClick={() => this.chooseWorkPlace()}>添加项目</button>
-      </div>
-
-    );
-  }
-}
+export default consumer(withRouter(Profile2));
--- src/renderer/context/consumer.tsx
+++ src/renderer/context/consumer.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Consumer, IState } from './index';
 
-export default function consumer<P>(WrappedComponent: React.ComponentType<P>): React.ComponentType<P> {
-  return class extends React.Component<P> {
-    render() {
-      return (
-        <Consumer>
-          {(contextStore: IState & any) => <WrappedComponent {...contextStore} {...this.props} />}
-        </Consumer>
-      );
-    }
-  };
+export default function consumer<P>(WrappedComponent: React.ComponentType<P>): React.SFC<IState> {
+  // eslint-disable-next-line react/display-name
+  return (props: IState) => (
+    <Consumer>
+      {(contextStore: IState & any) => <WrappedComponent {...contextStore} {...props} />}
+    </Consumer>
+  );
+  // return class HocConsumer extends React.Component<P> {
+  //   render() {
+  //     return (
+  //       <Consumer>
+  //         {(contextStore: IState & any) => <WrappedComponent {...contextStore} {...this.props} />}
+  //       </Consumer>
+  //     );
+  //   }
+  // };
 }
